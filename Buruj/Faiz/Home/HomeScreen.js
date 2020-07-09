6527ffdab22d3fd93cd00db68db112e71f6faad8
syafiqzepher <megabrain007@gmail.com>
2020-07-10 01:01:40 +0800

    Improvement

---
+++ Buruj/Faiz/Home/HomeScreen.js
@@ -0,0 +1,296 @@
+import React, {Component} from 'react';
+import { Text,View,TouchableOpacity,StyleSheet,Image,StatusBar,ScrollView} from "react-native";
+import {Icon}from 'react-native-elements';
+import BackgroundCarousel from '../../components/BackgroundCarousel';
+import Video from 'react-native-video';
+import { SliderBox } from "react-native-image-slider-box";
+
+const images = [
+  "https://gdurl.com/W736",
+  "https://gdurl.com/pFy6",
+  "https://gdurl.com/PogZ",
+  "https://gdurl.com/KphH"
+];
+
+export default class HomeScreen extends Component {
+  
+
+  constructor(props){
+    super(props);
+
+    this.state={
+    repeat: true,
+    rate: 1,
+    volume: 1,
+    muted: false,
+    resizeMode: 'cover', // video's mode: none, cover, stretch, contain
+    duration: 0.0, // video's duration set on event onLoad
+    currentTime: 0.0, // set on event onProgress
+    paused: false, // check if video is pausing or not
+    rateText: '1.0', // rate value in component Picker
+    pausedText: 'Play', // view to user: 'Play' - when video is pausing, 'Pause' - when video is playing
+    hideControls: false, // hide control button when video is playing and show it when user clicks on video
+    };
+
+    this.state = {
+      images: [
+        "https://gdurl.com/Eutu",
+        "https://gdurl.com/eyt3",
+        "https://gdurl.com/HXkb",
+        "https://gdurl.com/Zosh", // Network image
+      ]
+    };
+  }
+
+  onLoad=(data)=> {
+    this.setState({duration: data.duration})
+  }
+
+  onEnd=() => {
+    this.setState({ paused: false});
+    this.video.seek(0);  
+  }
+
+  render() {
+    return (
+      <ScrollView>
+      <View style={styles.screen}>
+        <StatusBar backgroundColor = "#141414" barStyle = "light-content"/>
+      <View style={styles.sliderContainer}>
+        {<BackgroundCarousel images = {images}/>}
+      </View>
+
+      <View style={{backgroundColor: '#ecf0f1'}}>
+        <Text style={{padding: 18, textAlign: 'center', fontWeight: 'bold'}}>READY STOCK ARRIVED WITHIN 1-3 DAYS ONLY</Text>
+      </View>
+
+      <View style={styles.imageContainerFirst}>
+        <View style={styles.videoContainer}>
+
+          <Video
+                ref={(ref) => {this.video = ref}}     
+                source={{uri: 'https://gdurl.com/whm7'}}
+                repeat={this.state.repeat}
+                rate={this.state.rate}
+                volume={this.state.volume}
+                muted={this.state.muted}
+                resizeMode={this.state.resizeMode}   
+                paused={this.state.paused}
+                onLoad={this.onLoad}
+                onProgress={this.onProgress}
+                onEnd={this.onEnd}
+                poster="https://gdurl.com/dpAf" // uri to an image to display until the video plays
+                posterResizeMode="stretch"              // Poster resize mode. One of contain (default), cover, stretch, center, repeat
+                // posterStyles={{resizeMode: 'cover'}} 
+                style={styles.backgroundVideo}
+              />
+        </View>
+        </View>
+
+        <View style={{marginTop: 10, marginBottom: 25,}}>
+        <View>
+        <Text style={{fontWeight: 'bold', fontSize: 35, textAlign: 'center', marginVertical: 20}}>BURUJ'S FUNCTIONS</Text>
+        <Image
+        source={{uri: 'https://gdurl.com/auap'}}
+        style={{width: "100%", height: 370, resizeMode: 'center', marginBottom: 25,}}/>
+        </View>
+        
+        <View style={{marginLeft: -215}}>
+        <View style={styles.groupfx}>
+        <Image
+        source={{uri: 'https://gdurl.com/YDq9'}}
+        style={styles.iconFx}/>
+        <View style={{marginHorizontal: -115,}}></View>
+        <Text style={styles.textfx}>World Prayer Times</Text>
+        </View>
+        
+        <View style={styles.groupfx}>
+        <Image
+        source={{uri: 'https://gdurl.com/aJdF'}}
+        style={styles.iconFx}/>
+        <View style={{marginHorizontal: -115,}}></View>
+        <Text style={styles.textfx}>Automatic Qibla Compass</Text>
+        </View>
+
+        <View style={styles.groupfx}>
+        <Image
+        source={{uri: 'https://gdurl.com/AT2bw'}}
+        style={styles.iconFx}/>
+        <View style={{marginHorizontal: -115,}}></View>
+        <Text style={styles.textfx}>Set Your Name</Text>
+        </View>
+        
+        <View style={styles.groupfx}>
+        <Image
+        source={{uri: 'https://gdurl.com/oSZJ'}}
+        style={styles.iconFx}/>
+        <View style={{marginHorizontal: -115,}}></View>
+        <Text style={styles.textfx}>Alarm Azan (Beep Sound)</Text>
+        </View>
+
+        <View style={styles.groupfx}>
+        <Image
+        source={{uri: 'https://gdurl.com/HFeF'}}
+        style={styles.iconFx}/>
+        <View style={{marginHorizontal: -115,}}></View>
+        <Text style={styles.textfx}>Digital Quran Bookmarker</Text>
+        </View>
+        </View>
+        </View>
+
+        <Image
+        source={{uri: 'https://gdurl.com/Xtjp'}}
+        style={{width: '100%', height: 180, resizeMode: 'stretch', marginVertical: 20}}/>
+
+        <Text style={{fontWeight: 'bold', fontSize: 35, textAlign: 'center',}}>MID-YEAR SALE</Text>
+
+        <View style={styles.innerFlex}>
+        <View>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('NewsScreen')}>
+        <View style={styles.imageContainer2}>
+          <Image
+          source={{uri: 'https://gdurl.com/hEVf'}}
+           style={{width: 170, height: 170, resizeMode: 'center'}}/>
+          <View style={styles.textContainer}>
+            <Text style={styles.textImage}>Find Out More</Text>
+          </View>
+        </View>
+        </TouchableOpacity>
+        </View>
+
+        <View style={{marginHorizontal: 15}}></View>
+        
+        <View>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('News2Screen')}>
+        <View style={styles.imageContainer2}>
+        <Image
+        source={{uri: 'https://gdurl.com/NH5X'}}
+           style={{width: 170, height: 170, resizeMode: 'center'}}/>
+          <View style={styles.textContainer}>
+            <Text style={styles.textImage}>Find Out More</Text>
+          </View>
+        </View>
+        </TouchableOpacity>
+        </View>
+        </View>
+
+        <View style={styles.imageContainerLast}>
+        <View>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('News3Screen')}>
+        <View style={styles.imageContainer1}>
+        <Image
+        source={{uri: 'https://gdurl.com/LOpr'}}
+           style={{width: "100%", height: 370, resizeMode: 'center'}}/>
+          <View style={styles.textContainer2}>
+            <Text style={styles.textImage2}>Find Out More</Text>
+          </View>
+        </View>
+        </TouchableOpacity>
+        </View>
+        </View>
+
+        <View>
+        <Text style={{fontWeight: 'bold', fontSize: 35, textAlign: 'center', marginTop: 10, marginBottom: 30,}}>BURUJ'S TESTIMONIALS</Text>
+        </View>
+
+        <SliderBox
+        images={this.state.images}
+        dotColor="#ffdc00"
+        inactiveDotColor="#90A4AE"
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 15,
+          padding: 0,
+          margin: 0
+        }}
+        paginationBoxVerticalPadding={20}
+        autoplay
+        circleLoop
+        ImageComponentStyle={{width: '100%', height: 410, resizeMode: 'cover',}}
+        />
+
+        <View style={{marginTop: 50}}></View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  screen: {
+    justifyContent: 'center',
+  },
+  sliderContainer: {
+  },
+  imageContainerFirst: {
+    alignItems: 'center',
+  },
+  imageContainerLast: {
+    marginBottom: 30,
+  },
+  imageContainer1: {
+    alignItems: 'center',
+  },
+  videoContainer: {
+    width: "100%",
+    height: 415,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  imageContainer2: {
+    alignItems: 'center',
+  },
+  textContainer: {
+    borderWidth: 1,
+    borderColor: "#b2babb",
+    borderTopWidth: 0,
+    width: '100%',
+  },
+  textContainer2: {
+    borderWidth: 1,
+    borderColor: "#b2babb",
+    borderTopWidth: 0,
+  },
+  textImage: {
+    textAlign: 'center',
+    color:'black',
+    fontWeight: 'bold',
+    fontSize: 15,
+    paddingHorizontal: 28,
+    paddingVertical: 10,
+  },
+  textImage2: {
+    textAlign: 'center',
+    color:'black',
+    fontWeight: 'bold',
+    fontSize: 15,
+    paddingHorizontal: 135,
+    paddingVertical: 10,
+  },
+  innerFlex: {
+    justifyContent: 'center',
+    marginVertical: 30,
+    flexDirection: 'row'
+  },
+  textfx: {
+    textAlign: 'center',
+    fontSize: 25,
+  },
+  iconFx: {
+    width: '85%',
+    height: 30,
+    resizeMode: 'center',
+  },
+  groupfx: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 9,
+  }
+});
---
+++ Buruj/Faiz/Tutorial/tutorial3.js
@@ -0,0 +1,198 @@
+import React, {Component} from 'react';
+import {View,StyleSheet, Text, ScrollView} from "react-native";
+import Video from 'react-native-video';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Icon} from 'react-native-elements';
+
+export default class Tutorial3Screen extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+    repeat: false,
+    rate: 1,
+    volume: 1,
+    muted: false,
+    resizeMode: 'stretch', // video's mode: none, cover, stretch, contain
+    duration: 0.0, // video's duration set on event onLoad
+    currentTime: 0.0, // set on event onProgress
+    paused: false, // check if video is pausing or not
+    rateText: '1.0', // rate value in component Picker
+    pausedText: 'Play', // view to user: 'Play' - when video is pausing, 'Pause' - when video is playing
+    hideControls: false, // hide control button when video is playing and show it when user clicks on video
+    };
+  }
+
+  onLoad=(data)=> {
+    this.setState({duration: data.duration})
+  }
+
+  onPress=(data)=> {
+    this.setState({currentTime: data.currentTime});
+  }
+
+  onEnd=() => {
+    this.setState({pausedText: 'Play', paused: true});
+    this.video.seek(0);
+  }
+
+  //Change Header 
+  static navigationOptions = {
+    headerTitle: 'Tutorial',
+    headerRight:<Icon
+    name='shopping-cart'
+    color='#FFFFFF'
+    onPress={() => alert('Cart Page is on progress')} />,
+    headerLeft:<View/>,
+    headerStyle: {
+      backgroundColor: '#141414',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      alignSelf: 'center',
+      textAlign: "center",
+      justifyContent: 'center',
+      flex: 1,
+      textAlignVertical: 'center',
+      color:'#FFFFFF',
+      fontSize: 15,
+      fontWeight:'bold',
+      },
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.screen}>
+
+          <View style={styles.container}>
+
+            <View style={styles.textContainer}>
+              <Text style={styles.titleText}>SET AZAN ALARM</Text>
+            </View>
+
+            <View style={styles.videoContainer}>
+              <Video
+                ref={(ref) => {this.video = ref}}     
+                source={{uri: 'https://gdurl.com/tYX1'}}
+                repeat={this.state.repeat}
+                rate={this.state.rate}
+                volume={this.state.volume}
+                muted={this.state.muted}
+                resizeMode={this.state.resizeMode}   
+                paused={this.state.paused}
+                onLoad={this.onLoad}
+                onProgress={this.onProgress}
+                onEnd={this.onEnd}
+                poster="https://gdurl.com/KCelx" // uri to an image to display until the video plays
+                posterResizeMode="stretch"              // Poster resize mode. One of contain (default), cover, stretch, center, repeat
+                // posterStyles={{resizeMode: 'cover'}} 
+                style={styles.backgroundVideo}
+              />
+            </View>
+            <View style={styles.frameContainer}></View>
+
+            <View style={{alignItems: 'center'}}>
+            <TouchableOpacity style={styles.buttonPlay} onPress={() => this.setState({paused: !this.state.paused})}>
+            <Text style={{padding: 8, color: 'white', fontSize: 15, fontWeight: 'bold'}}>PLAY/PAUSE</Text>
+            </TouchableOpacity>
+            </View>
+          </View>
+          
+          <View style={styles.container2}>
+            <View style={styles.textContainer}>
+              <Text style={styles.titleText}>WATCH MORE VIDEOS</Text>
+            </View>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial')}>
+              <Text style={styles.buttonText}>Buruj Watches Features</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial2')}>
+              <Text style={styles.buttonText}>Qibla Calibration</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial4')}>
+              <Text style={styles.buttonText}>Quran Bookmarker</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial5')}>
+              <Text style={styles.buttonText}>Set Your Name</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial6')}>
+              <Text style={styles.buttonText}>DST Select</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.replace('Tutorial7')}>
+              <Text style={styles.buttonText}>Azan Safety</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{alignItems: 'center',}}
+           onPress={() => this.props.navigation.replace('Tutorial8')}>
+             <Text style={styles.buttonText}>Set Your Country</Text>
+            </TouchableOpacity>
+          </View>
+       </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    justifyContent: 'center',
+  },
+  container: {
+  },
+  container2: {
+    marginTop: 30
+  },
+  videoContainer: {
+    width: "100%",
+    height: 180,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  frameContainer: {
+    width: '100%',
+    backgroundColor: '#16a085',
+    height: 40
+  },
+  buttonPlay: {
+    backgroundColor: '#909497',
+    alignItems: 'center',
+    marginTop: 5,
+    width: '30%',
+    borderRadius: 3
+  },
+  titleText: {
+      alignSelf: 'center',
+      textAlign: "center",
+      justifyContent: 'center',
+      flex: 1,
+      fontSize: 17,
+      textAlignVertical: 'center',
+      color:'white',
+      padding: 5,
+      fontWeight: 'bold'
+  },
+  textContainer: {
+    padding: 5,
+    backgroundColor: '#16a085'
+  },
+  buttonContainer: {
+    borderBottomWidth: 1,
+    borderColor: '#b2babb',
+    alignItems: 'center',
+  },
+  buttonText: {
+    padding: 12,
+    fontSize: 17,
+  }
+});
